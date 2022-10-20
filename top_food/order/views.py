ffbe19ce8e2c6d6a624c53c092fc76a59813738d
YOUR_NAME <YOUR_EMAIL>
2022-10-20 17:12:57 +0300

    Commit#6 create asynco def create_pdf_check_async and save_check_in_db_async

--- top_food/order/views.py
+++ top_food/order/views.py
@@ -1,6 +1,5 @@
 import os
 import re
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from .models import Check, Printer
@@ -8,20 +7,41 @@ from .forms import OrderForm
 import json
 from fpdf import FPDF
 from django.http import JsonResponse
-
+from asgiref.sync import sync_to_async
 import asyncio
+import time
 
 
-def create_pdf_check(check, order):
-    pdf = FPDF(format='A5')
-    pdf.add_page()
-    pdf.add_font('DejaVu', '', 'media/font/DejaVuSansCondensed.ttf', uni=True)
-    pdf.set_font('DejaVu', '', 14)
-    title = f'Ваше замовлення № {check.order_number}'
-    pdf.cell(200, 10, txt=title, ln=1, align='L')
-    for key, value in order.items():
-        pdf.cell(200, 10, txt=f"{key} - {value}", ln=1, align='L')
-    pdf.output(f'media/pdf/{check.order_number}_{check.type}.pdf')
+def create_pdf_check_async(order, order_number, order_point):
+    printers = Printer.objects.filter(point_id=order_point)
+    for printer in printers:
+        pdf = FPDF(format='A5')
+        pdf.add_page()
+        pdf.add_font('DejaVu', '', 'media/font/DejaVuSansCondensed.ttf', uni=True)
+        pdf.set_font('DejaVu', '', 14)
+        title = f'Ваше замовлення № {order_number}'
+        pdf.cell(200, 10, txt=title, ln=1, align='L')
+        for key, value in order.items():
+            pdf.cell(200, 10, txt=f"{key} - {value}", ln=1, align='L')
+        pdf.output(f'media/pdf/{order_number}_{printer.check_type}.pdf')
+        print("---------The file is ready--------------")
+        time.sleep(3)
+
+
+def save_check_in_db_async(order_point, json_data, order_number):
+    printers = Printer.objects.filter(point_id=order_point)
+    for printer in printers:
+        check_to_save = Check(
+            printer_id=printer,
+            api_key=printer.api_key,
+            type=printer.check_type,
+            order=json_data,
+            pdf_file=f'{order_number}_{printer.check_type}.pdf',
+            order_number=order_number
+        )
+        check_to_save.save()
+        print("Save data in db")
+        time.sleep(2)
 
 
 def get_order_number():
@@ -30,7 +50,7 @@ def get_order_number():
     directory = 'media/pdf'
     files = os.listdir(directory)
     if not files:
-        return None
+        return 1
     for file in files:
         order = re.search(order_numbers_pattern, file)
         order_numbers.add(int(order.group()))
@@ -43,40 +63,45 @@ def order_menu(request):
     return render(request, 'order.html', {'order': order})
 
 
-def order_is_accepted(request):
+async def order_is_accepted_async(request):
+    start = time.time()
     order_dict = {
         'first_dish': request.POST['first_dish'],
         'second_dish': request.POST['second_dish'],
         'drinks': request.POST['drinks'],
         'other': request.POST['other'],
     }
+    order_point = int(request.POST['order_point'])
     json_data = json.dumps(order_dict)
-    printers = Printer.objects.all()
     order_number = get_order_number()
-    if not printers:
-        return render(request, 'no_printer.html', {})
-    for printer in printers:
-        if printer.point_id == int(request.POST['order_point']):
-            check_to_save = Check(
-                printer_id=printer,
-                api_key=printer.api_key,
-                type=printer.check_type,
-                order=json_data,
-                pdf_file=f'{order_number}_{printer.check_type}.pdf',
-                order_number=order_number
-            )
-            check_to_save.save()
-            create_pdf_check(order=order_dict, check=check_to_save)
+# asynco var#1
+    loop = asyncio.get_event_loop()
+    async_function1 = sync_to_async(save_check_in_db_async, thread_sensitive=False)
+    async_function2 = sync_to_async(create_pdf_check_async, thread_sensitive=False)
+    loop.create_task(async_function1(
+        order_point=order_point,
+        json_data=json_data,
+        order_number=order_number))
+    loop.create_task(async_function2(
+        order=order_dict,
+        order_number=order_number,
+        order_point=order_point))
+# asynco var#2 (decoration @sync_to_async to funk)
+    # task1 = asyncio.ensure_future(save_check_in_db_async(
+    #     order_point=order_point,
+    #     json_data=json_data,
+    #     order_number=order_number)
+    # )
+    # task2 = asyncio.ensure_future(create_pdf_check_async(
+    #     order=order_dict,
+    #     order_number=order_number,
+    #     order_point=order_point)
+    # )
+
+    # await asyncio.wait([task1, task2])
+    total_time = (time.time() - start)
+    print(total_time)
     return render(request, 'form_done.html', {'order_dict': order_dict})
-
-
-def get_files_for_printer(request):
-    checks = Check.objects.all()
-    for check in checks:
-        if check.status == 'Rendered':
-            check.status = 'Printed'
-            check.save()
-    return render(request, 'get_files_for_printer.html')
 
 
 class PrinterView(View):
--- top_food/printer.py
+++ top_food/printer.py
@@ -1,18 +1,26 @@
 import argparse
 import time
 import requests
-
+import asyncio
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-id', '--id', type=int, help='Printer operation')
 args = parser.parse_args()
-print(f'Принтер работает')
-print(args.id)
+# print(f'Принтер работает')
+# print(args.id)
 
 
-while True:
-    response = requests.get(f'http://127.0.0.1:8000/get_files_for_printer/{args.id}')
-    time.sleep(5)
-    print(response)
-    print(response.json())
+# while True:
+#     response = requests.get(f'http://127.0.0.1:8000/get_files_for_printer/{args.id}')
+#     time.sleep(5)
+#     print(response)
+#     print(response.json())
+
+async def async_function(a):
+    while True:
+        print(a)
+        await asyncio.sleep(1/10)
+        a = a + 1
+
+asyncio.run(async_function(a=1))
 
